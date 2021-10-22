aac47562136913d88868198971835908cd05bc62
ManiMaran610 <manimaran.0610@gmail.com>
2021-10-22 14:10:50 +0530

    Add project files.

---
+++ SampleWebApp/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SampleWebApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server" BorderColor ="Red" BackColor="WindowFrame"></asp:GridView>
+      
+    </form>
+</body>
+</html>
---
+++ SampleWebApp/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SampleWebApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String ConnString = ConfigurationManager.ConnectionStrings["MYDB"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(ConnString))
+            {
+
+                SqlCommand cmd = new SqlCommand("Select * from programmer", connection);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                GridView1.DataSource = reader;
+                GridView1.DataBind();
+
+
+
+            }
+        }
+        }
+}
